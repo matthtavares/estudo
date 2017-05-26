fc4f799901332eb3b3c6425507896c543e4eb0b9
Mateus Tavares <mateus460@gmail.com>
2017-05-26 15:57:41 -0300

    JS updated

--- JS/Order.class.js
+++ JS/Order.class.js
@@ -1,17 +1,27 @@
 class Order {
 
   constructor () {
-    this.products = new Array();
+    this.products = new Array(); // Variável que armazenará os produtos
   }
 
+  /**
+   * Este método serve para adicionar produtos
+   * ao pedido.
+   *
+   * @param product Product
+   */
   addProduct ( product ) {
     if( !(product instanceof Product) ) throw Error('Não é um produto válido!');
 
-    this.products.push(product);
+    this.products.push(product); // Adiciona o produto ao array
   }
 
+  /**
+   * Este método retorna o valor do pedido.
+   */
   total () {
     let total = 0;
+    // Percorre o array de produtos
     for(let p of this.products){
       total += p.price;
     }
--- JS/Product.class.js
+++ JS/Product.class.js
@@ -1,13 +1,20 @@
 class Product {
 
+  /**
+   * Deve-se informar o id, titulo/descrição e preço do produto.
+   *
+   * @param id Number
+   * @param product String
+   * @param price Number
+   */
   constructor ( id, product, price ){
     if( typeof id != "number" ) throw Error('Parâmetro "id" inválido. Apenas números inteiros.');
     if( typeof product != "string" ) throw Error('Parâmetro "product" inválido. Apenas inteiros.');
     if( typeof price != "number" ) throw Error('Parâmetro "price" inválido. Apenas números reais.');
 
-    this.id = id;
+    this.id = Number.parseInt(id);
     this.product = product;
-    this.price = price;
+    this.price = Number.parseFloat(price);
   }
 
 }

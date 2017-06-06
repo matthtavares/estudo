891aaf1effcd7abe90c4c31635b61a38953ff2a0
Mateus Tavares <mateus460@gmail.com>
2017-06-06 16:09:56 -0300

    Update Product.class.js

--- JS/Product.class.js
+++ JS/Product.class.js
@@ -9,7 +9,7 @@ class Product {
    */
   constructor ( id, product, price ){
     if( typeof id != "number" ) throw Error('Parâmetro "id" inválido. Apenas números inteiros.');
-    if( typeof product != "string" ) throw Error('Parâmetro "product" inválido. Apenas inteiros.');
+    if( typeof product != "string" ) throw Error('Parâmetro "product" inválido. Apenas string.');
     if( typeof price != "number" ) throw Error('Parâmetro "price" inválido. Apenas números reais.');
 
     this.id = Number.parseInt(id);
